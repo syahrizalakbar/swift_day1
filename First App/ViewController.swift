97200bd4ea1e17ba5ea57a9ecd1e65fc527b7785
Syahrizal Akbar <syahrizal@udacoding.com>
2020-10-05 20:50:59 +0700

    Initial Commit

---
+++ First App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  First App
+//
+//  Created by IDOUDA on 05/10/20.
+//  Copyright © 2020 IDOUDA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
