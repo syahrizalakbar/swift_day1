fa007db2924c634fc78348e445ec116900c83e8b
Syahrizal Akbar <syahrizal@udacoding.com>
2020-10-05 21:29:20 +0700

    belajar swift 1, storyboard

--- First App/ViewController.swift
+++ First App/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var input1: UITextField!
+    @IBOutlet weak var input2: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func actionButton(_ sender: UIButton) {
+        // tangkap nilai textField
+        let x = input1.text
+        let y = input2.text
+        
+        let alert = UIAlertController(title: x, message: y, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default) { (<#UIAlertAction#>) in
+            
+            print("ini klik ok button action")
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true) {
+            
+        }
+    }
+    
 }
 
